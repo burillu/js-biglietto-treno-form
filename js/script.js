97121f34892f5754bb289b440a97da0b48e8009a
burillu <burillu90@hotmail.it>
2023-10-10 16:50:20 +0200

    function click in button work

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const tripkm = document.getElementById('tripKm');
 const age = document.getElementById('age');
 const submit = document.querySelector('.btn-primary');
+// console.log(submit);
 const unitPrice = 0.21;
 const discountMin = 1 - 20/100;
 const discountOver = 1 - 40/100;
+let basePrice= unitPrice * tripkm;
+
+submit.addEventListener('click', function(){
+    let valAge = age.value;
+    let valtripKm = tripkm.value;
+    console.log(valtripKm);
+    console.log(valAge);
+
+})
