c4b45653adeaa71533acc9929183a0ba42649305
burillu <burillu90@hotmail.it>
2023-10-10 16:30:37 +0200

    created base constant in js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const tripkm = document.getElementById('tripKm');
+const age = document.getElementById('age');
+const submit = document.querySelector('.btn-primary');
+const unitPrice = 0.21;
+const discountMin = 1 - 20/100;
+const discountOver = 1 - 40/100;
